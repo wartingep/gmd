e9cf27a44acb664d9e86dccc4ca84ec381a93539
Erik Eelde <erik.eelde@easypark.net>
2023-09-21 14:23:47 +0200

    2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,11 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    java
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
